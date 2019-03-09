bc88d405f04d6679b7e1cc437e266d25312ebf77
Derek Clark <clarkd2013@gmail.com>
2019-03-09 20:20:36 +0000

    Added profiles create option

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,5 +6,22 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
   
+  #POST request at users/:user_id/profile
+  def create
+    # Ensure we have the user that is filling out the form
+    @user = User.find( params[:user_id])
+    # Create profile that is linked to the specific user
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
     
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
